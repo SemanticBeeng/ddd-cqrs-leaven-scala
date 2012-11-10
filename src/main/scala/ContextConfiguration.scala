921ed7cee75487bcd6a85212257a4dca9a301efc
Krzysztof Ciesielski <panciesielski@gmail.com>
2012-11-10 21:13:45 +0100

    Added comment to context configuration

--- src/main/scala/ContextConfiguration.scala
+++ src/main/scala/ContextConfiguration.scala
@@ -1,5 +1,8 @@
 import pl.com.bottega.erp.sales.presentation.mongo.MongoProductFinder
 
+/**
+ * Represents configuration of context dependencies.
+ */
 object ContextConfiguration {
 
       lazy val productFinder = new MongoProductFinder
