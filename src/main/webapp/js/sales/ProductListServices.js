6195fa8ee8b5906194c9bba21f5319c9e34f2422
Krzysztof Ciesielski <panciesielski@gmail.com>
2012-11-16 21:33:14 +0100

    Removed unnecessary param

--- src/main/webapp/js/sales/ProductListServices.js
+++ src/main/webapp/js/sales/ProductListServices.js
@@ -1,6 +1,6 @@
 angular.module('productListServices', ['ngResource']).
     factory('Products', function($resource) {
         return $resource('products/', {}, {
-            query: {method: 'GET', params: {containsText: 'a'}, isArray: false}
+            query: {method: 'GET', params: {}, isArray: false}
         });
     });
